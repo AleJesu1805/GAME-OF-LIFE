3d4856fe64ea2cdb8559728a069ca1c4b401dfe8
AleJesu1805 <alexanderj150823@gmail.com>
2026-09-15 11:02:06 -0400

    se usa un set de objetos para renderizar las celulas, aun no hay actualizacion de posiciones

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("canvas");
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = 10;
+canvas.height = 10;
 const ctx = canvas.getContext("2d");
 
 const colorNumero = {
@@ -8,77 +8,34 @@ const colorNumero = {
   1: "#4e590f",
 };
 
-let matriz = (ancho, alto) => {
-  let arr = [];
-  for (let y = 0; y < alto; y++) {
-    let filas = [];
-    arr.push(filas);
-    for (let x = 0; x < ancho; x++) {
-      filas.push(0);
-    }
+const celulas = new Set();
+
+celulas.add({ x: 1, y: 2 });
+celulas.add({ x: 9, y: 2 });
+celulas.add({ x: 4, y: 4 });
+celulas.add({ x: 3, y: 3 });
+celulas.add({ x: 2, y: 5 });
+
+function drawCelula(pos = { x: 1, y: 2 }) {
+  ctx.fillStyle = colorNumero[1];
+  ctx.fillRect(pos.x, pos.y, 1, 1);
+}
+
+function drawAllCelulas() {
+  for (const celula of celulas) {
+    drawCelula(celula);
   }
-  return arr;
-};
+}
 
-let render = (matriz) => {
-  let tamTile = Math.round(canvas.height / matriz.length);
-  for (let y = 0; y < matriz.length; y++) {
-    for (let x = 0; x < matriz[0].length; x++) {
-      let posX = x * tamTile;
-      let posY = y * tamTile;
-
-      ctx.fillStyle = colorNumero[matriz[y][x]];
-      ctx.fillRect(posX, posY, tamTile, tamTile);
-    }
+function updateCelulas() {
+  for (let i = 0; i < celulas.size; i++) {
+    celulas.forEach((cel) => {
+      console.log(cel);
+    });
   }
-};
+}
 
-let actualizar = (array) => {
-  for (let y = 0; y < array.length; y++) {
-    for (let x = 0; x < array.length; x++) {
-      if (array[y][x] === 1) {
-        let celulasVecinas = [
-          array[y][x + 1],
-          array[y][x - 1],
-          array[y + 1][x],
-          array[y - 1][x],
-          array[y + 1][x + 1],
-          array[y - 1][x - 1],
-          array[y + 1][x - 1],
-          array[y - 1][x + 1],
-        ];
-        let cont = 0;
-
-        celulasVecinas.forEach((cel, i) => {
-          if (cel === 1) {
-            cont++;
-            if (cont < 2 || cont > 4) {
-              array[y][x] = 0;
-            } else if (cont === 3) {
-              array[y][x] = 1;
-            }
-          }
-        });
-
-        console.log(celulasVecinas, cont);
-      }
-    }
-  }
-};
-
-let array = matriz(20, 20);
-array[10][10] = 1;
-array[11][10] = 1;
-array[9][10] = 1;
-array[10][11] = 1;
-array[10][13] = 1;
-array[10][12] = 1;
-array[10][14] = 1;
-array[10][15] = 1;
-array[10][16] = 1;
-array[10][17] = 1;
-array[10][18] = 1;
-render(array);
+updateCelulas();
 
 const fps = 10;
 const frameDuration = 1000 / fps;
@@ -88,9 +45,8 @@ function gameLoop(tiempoActual) {
   if (delta < frameDuration) return;
   ultimoTiempo = tiempoActual - (delta % frameDuration);
 
+  drawAllCelulas();
   requestAnimationFrame(gameLoop);
-  actualizar(array);
-  render(array);
 }
 
 setTimeout(() => {
